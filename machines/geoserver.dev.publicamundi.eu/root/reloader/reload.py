505a246c44c53963754ed7001308c7c32449c5c1
drmalex07 <alexakis@imis.athena-innovation.gr>
2014-11-28 00:59:26 +0200

    Rewrote geoserver reload script as a Celery-compliant webhook

--- machines/geoserver.dev.publicamundi.eu/root/reloader/reload.py
+++ machines/geoserver.dev.publicamundi.eu/root/reloader/reload.py
@@ -2,34 +2,55 @@ from wsgiref.simple_server import make_server
 import pycurl
 import json
 
-NUM_BACKEND_SERVERS = 5
+PORT = 8000
+
+BACKEND_RELOAD_URL = 'http://%(host)s/geoserver/rest/reload'
+NUM_BACKEND_SERVERS = 4
+FRONTEND_SERVER_PORT = 8080
+
+USERPWD = 'admin:password'
 
 def application(environ, start_response):
-    global NUM_BACKEND_SERVERS
+    
+    # Iterate on backend servers and force them reload
 
     result = {}
-    data = ''
-    for i in range(1, NUM_BACKEND_SERVERS):
+    details = {}
+    success = True
+    for i in range(1, NUM_BACKEND_SERVERS + 1):
+        name = 'backend-%d' %(i) 
+        host = 'localhost:%d' % (FRONTEND_SERVER_PORT + i)
+        # Prepare a cURL request
         c = pycurl.Curl()
-        host = "localhost:%d" % (8080 + i)
-        c.setopt(pycurl.URL, "http://%s/geoserver/rest/reload" % (host))
+        c.setopt(pycurl.URL,  BACKEND_RELOAD_URL % dict(host=host))
         c.setopt(pycurl.POST, 1)
-        c.setopt(pycurl.USERPWD, 'admin:password')
-        c.setopt(pycurl.POSTFIELDS, data)
+        c.setopt(pycurl.USERPWD, USERPWD)
+        c.setopt(pycurl.POSTFIELDS, '')
+        # Perform, trap errors
         try:
             c.perform()
             response_code = int(c.getinfo(c.RESPONSE_CODE))
             if response_code != 200:
-                result[i] = dict(success=False, message='Failed (HTTP %d)' % (response_code))
-            else:
-                result[i] = dict(success=True)
+                success = False
+                details[name] = dict(message='Failed (HTTP %d)' % (response_code)) 
         except pycurl.error as ex:
-            result[i] = dict(
-                success=False, message='Failed (pycurl %d: %s)' % (ex.args))
+            success = False
+            details[name] = dict(message='Failed (pycurl %d: %s)' % (ex.args))
+    
+    # Build a Celery-compliant webhook response with an overall status
+
+    if success:
+        result['status'] = 'success'
+        result['retval'] = None
+    else:
+        result['status'] = 'failure'
+        result['reason'] = 'Failed to reload backend servers'
+        result['details'] = details
+
     start_response('200 OK', [('Content-type', 'application/json')])
     return [json.dumps(result)]
 
-httpd = make_server('', 8000, application)
-print "Serving HTTP on port 8000..."
+httpd = make_server('', PORT, application)
+print "Serving HTTP on port %d..." %(PORT)
 httpd.serve_forever()
 
