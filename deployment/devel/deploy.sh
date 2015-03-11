34e375b7af628f84afd62cd44be805a015cfe0bf
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-03-12 01:03:16 +0200

    Added an ansible role for Solr (v3)

---
+++ deployment/devel/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Prepare
+#
+
+#ansible-playbook -i hosts.conf -v prepare.yml
+
+#
+# Setup ansible groups
+#
+
+#ansible-playbook -i hosts.conf -v -l catalog play.yml
+#ansible-playbook -i hosts.conf -v -l search-engine play.yml
+
