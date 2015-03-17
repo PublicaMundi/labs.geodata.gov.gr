cc379d0e633a3b49ad0cf87e1f4f3ffdf072d283
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-03-18 01:37:31 +0200

    Created another Ansible role: postgres9

--- deploy/devel/deploy.sh
+++ deploy/devel/deploy.sh
@@ -10,7 +10,8 @@ ansible-playbook -i hosts.conf --force-handlers -v prepare.yml
 # Run playbooks for various groups (of managed hosts)
 #
 
-ansible-playbook -i hosts.conf -v -l catalog play.yml
-
 ansible-playbook -i hosts.conf -v -l search-engine play.yml
 
+ansible-playbook -i hosts.conf -v -l database play.yml
+
+ansible-playbook -i hosts.conf -v -l catalog play.yml
