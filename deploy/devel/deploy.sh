96d4215c9e2e7abaf1fb0e8d9b3370a842f84a3c
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-04-19 23:30:45 +0300

    Minor change in ansible.cfg

--- deploy/devel/deploy.sh
+++ deploy/devel/deploy.sh
@@ -4,16 +4,16 @@
 # Prepare needed stuff at control machine (i.e. localhost)
 #
 
-ansible-playbook -i hosts.conf --force-handlers -v prepare.yml
+ansible-playbook --force-handlers -v prepare.yml
 
 #
 # Run playbooks for various groups (of managed hosts)
 #
 
-ansible-playbook -i hosts.conf -v -l monitor play.yml
+ansible-playbook -v -l monitor play.yml
 
-ansible-playbook -i hosts.conf -v -l search-engine play.yml
+ansible-playbook -v -l search-engine play.yml
 
-ansible-playbook -i hosts.conf -v -l database play.yml
+ansible-playbook -v -l database play.yml
 
-ansible-playbook -i hosts.conf -v -l catalog play.yml
+ansible-playbook -v -l catalog play.yml
