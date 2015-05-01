edf9f52f284dd8636f175c068c48b4aac4c7e238
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-01 21:14:30 +0300

    A working draft for prepare-admin-network playbook

--- deploy/prod/templates/hosts/admin.publicamundi.eu/etc/firewall/setup.d/nat-fwd-ssh.sh
+++ deploy/prod/templates/hosts/admin.publicamundi.eu/etc/firewall/setup.d/nat-fwd-ssh.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Provide DNAT for SSH to internal hosts
+# Provide SSH connectivity to internal hosts. Also, take care to filter SSH break-in attempts 
+# to those hosts
 
 {% for host in groups["internal"] -%}
-{%- set dport = hostvars[host].networking.nat.ssh.external_port| int -%}
-{%- if dport %}
-{%- set daddr = hostvars[host].networking.if.internal_ipv4.address -%}
-iptables -t nat -i ${public_ip4_iface} -A PREROUTING -j DNAT -p tcp -d ${public_ip4} --dport '{{dport}}' --to-destination '{{daddr}}:22'
-{% endif %} 
-{% endfor %}
+{%- set from_host = hostvars[host].networking.nat.ssh.external_host| default('') -%}
+{%- set from_port = hostvars[host].networking.nat.ssh.external_port| int(default=0) -%}
+{%- if (from_host == inventory_hostname) and (from_port > 0) %}
+{%- set to_addr = hostvars[host].networking.if.internal_ipv4.address -%}
+# Forward traffic to port {{from_port}} to host {{host}} 
+iptables -t nat -i ${public_ip4_iface} -A PREROUTING -j DNAT -p tcp -d ${public_ip4} --dport '{{from_port}}' --to-destination '{{to_addr}}:22'
+# Filter forwarded traffic to {{from_port}} for host {{host}}
+iptables -A FORWARD -i ${public_ip4_iface} -p tcp -m tcp \
+  --dport '{{from_port}}' -m state --state NEW -m recent --set --name 'SSH_FWD_{{from_port}}' --rsource 
+iptables -A FORWARD -i ${public_ip4_iface} -p tcp -m tcp \
+  --dport '{{from_port}}' -m state --state NEW -m recent --update --seconds 60 --hitcount 8 --name 'SSH_FWD_{{from_port}}' --rsource -j REJECT
+{% endif %}
 
+{% endfor %}
