7300fe64cb23f9ca92b1a4a4f2ea36fd9587f658
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-11 16:20:55 +0300

    Update firewall rules for admin node

--- deploy/prod/templates/hosts/admin.publicamundi.eu/etc/firewall/setup.d/nat-fwd-ssh.sh
+++ deploy/prod/templates/hosts/admin.publicamundi.eu/etc/firewall/setup.d/nat-fwd-ssh.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Provide SSH connectivity to internal hosts. Also, take care to filter SSH break-in attempts 
-# to those hosts
+# to those internal hosts
+
+{%- set forwarded_ports = [] %}
 
 {% for host in groups["internal"] -%}
 {%- set from_host = hostvars[host].networking.nat.ssh.external_host| default('') -%}
 {%- set from_port = hostvars[host].networking.nat.ssh.external_port| int(default=0) -%}
-{%- if (from_host == inventory_hostname) and (from_port > 0) %}
+{%- if (from_host == inventory_hostname) and (from_port > 0) and not (from_port in forwarded_ports) %}
 {%- set to_addr = hostvars[host].networking.if.internal_ipv4.address -%}
+{%- do forwarded_ports.append(from_port) %} 
 # Forward traffic to port {{from_port}} to host {{host}} 
 iptables -t nat -i ${public_ip4_iface} -A PREROUTING -j DNAT -p tcp -d ${public_ip4} --dport '{{from_port}}' --to-destination '{{to_addr}}:22'
 # Filter forwarded traffic to {{from_port}} for host {{host}}
