78daf871e643388eb9d310952cfa032c79ed99fb
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-14 17:18:52 +0300

    Reorganized deploy directory

--- deploy/devel/install-ansible-prereqs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -f ~/.ansible-prereqs-installed ]; then
-   apt-get install -y python
-   [ "$?" -eq 0 ] && touch ~/.ansible-prereqs-installed
-fi
--- deploy/devel/roles/ckan/README.md
+++
@@ -1,4 +0,0 @@
-README
-------
-
-This role (`ckan`) is responsible for setting-up CKAN application
