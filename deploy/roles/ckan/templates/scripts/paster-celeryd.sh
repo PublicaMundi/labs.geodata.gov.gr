c495376d1d9bb28da8f4f644ba0cbf7f1661df3c
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-29 15:53:26 +0300

    Moved CKAN role under ansible-roles project

--- deploy/roles/ckan/templates/scripts/paster-celeryd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PYENV_DIR={{ckan.pyenv_dir}}
-
-. ${PYENV_DIR}/bin/activate
-cd ${PYENV_DIR}/src/ckan && paster celeryd -c config.ini
-
--- deploy/roles/ckan/templates/scripts/paster-serve-ckan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PYENV_DIR={{ckan.pyenv_dir}}
-
-. ${PYENV_DIR}/bin/activate
-cd ${PYENV_DIR}/src/ckan && paster serve --reload config.ini
-
