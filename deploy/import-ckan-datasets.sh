a0908710d8637101cf1cbbb7f5781db6e4099709
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-06-14 01:24:16 +0300

    Finish playbook to import datasets into CKAN
    The imported datasets are also added in topic categories (groups).

--- deploy/import-ckan-datasets.sh
+++ deploy/import-ckan-datasets.sh
@@ -1,20 +1,24 @@
 #/bin/bash
 
+## Options
+
 host=admin.publicamundi.localdomain
 port=8090
-datasets_folder=files/groups/ckan/initialize/datasets/
+folder=files/groups/ckan/initialize/datasets/
+
+## Main
 
 screen_name=serve-folder-${RANDOM}
 
-# Start HTTP server at datasets folder
+folder=$(cd ${folder} && pwd)
 
-screen -dm -S ${screen_name} scripts/serve-folder.py -p ${port} -l ${host} ${datasets_folder}
+# Start HTTP server at datasets folder
+screen -dm -S ${screen_name} scripts/serve-folder.py -p ${port} -l ${host} ${folder}
 
 # Import datasets
-
-ansible-playbook -v -e datasets_folder=http://${host}:${port}/xml scratch.yml
+ansible-playbook -v \
+  -e web_folder=http://${host}:${port}/xml -e folder=${folder}/xml \
+  scratch.yml
 
 # Stop HTTP server
-
 screen -S ${screen_name} -X quit
-#echo 'Stopped screen: ' ${screen_name}
