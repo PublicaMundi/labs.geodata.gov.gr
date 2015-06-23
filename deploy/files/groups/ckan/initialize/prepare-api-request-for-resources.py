37ab851615f47f8801d66ee99d22ee4793a20d74
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-06-23 12:17:50 +0300

    Review import-ckan-datasets playbook (proper Host headers, additional resource metadata)

--- deploy/files/groups/ckan/initialize/prepare-api-request-for-resources.py
+++ deploy/files/groups/ckan/initialize/prepare-api-request-for-resources.py
@@ -6,6 +6,7 @@ import json
 import argparse
 import logging
 import hashlib
+import mimetypes
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,8 +14,9 @@ argp = argparse.ArgumentParser()
 argp.add_argument("inputs", metavar='INFILE', type=str, nargs=1)
 argp.add_argument("-b", "--resources-folder", dest='resources_folder', 
     default='resources', help=u'The base directory for resource files') 
+argp.add_argument("-l", "--limit", dest='limit', type=int) 
 
-def prepare_json_requests(infile, resources_folder):
+def prepare_json_requests(infile, resources_folder, limit=None):
     res = []
     
     with open(infile, 'r') as ifp:
@@ -40,19 +42,28 @@ def prepare_json_requests(infile, resources_folder):
             hexdigest = None
             with open(resource_path, 'r') as ifp:
                 hexdigest = hashlib.sha1(ifp.read()).hexdigest()
-        
+            
+            size = os.stat(resource_path).st_size
+
+            mimetype, encoding = mimetypes.guess_type(resource_path)
+            
             req_data = {
                 'package_id': package_id,
                 'format': resource_type,
                 'name': resource_title,
+                'mimetype': mimetype,
                 'hash': hexdigest,
+                'size': size,
                 'upload': resource_path,
             }
             res.append(req_data)
+
+            if limit and limit == len(res):
+                break
 
     print json.dumps(res)
 
 if __name__ == '__main__':
     args = argp.parse_args()
-    prepare_json_requests(args.inputs[0], args.resources_folder)
+    prepare_json_requests(args.inputs[0], args.resources_folder, args.limit)
 
