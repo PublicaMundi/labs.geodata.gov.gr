2a2f0bd49f45d7bbac267e98baa18aa5c3318375
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-12-02 19:04:57 +0100

    Fixed expired key issue

--- deploy/ansible_prereqs.sh
+++ deploy/ansible_prereqs.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+rm /etc/apt/sources.list
+cat << EOF > "/etc/apt/sources.list"
+deb http://ftp.gr.debian.org/debian/ wheezy main
+deb-src http://ftp.gr.debian.org/debian/ wheezy main
+deb http://security.debian.org/ wheezy/updates main
+deb-src http://security.debian.org/ wheezy/updates main
+deb http://ftp.gr.debian.org/debian/ wheezy-updates main
+deb-src http://ftp.gr.debian.org/debian/ wheezy-updates main
+EOF
+
 if [ ! -f /root/.ansible_prereqs_installed ]; then
         apt-get update
         apt-get install -y python python-apt python-pycurl sshpass
